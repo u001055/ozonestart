ed966949bb65273776f51b06b19a2a6c7687eb92
Oleg <52830599+u001055@users.noreply.github.com>
2020-01-24 23:21:37 +0300

    remove from cart

--- src/index.js
+++ src/index.js
@@ -45,15 +45,35 @@ cards.forEach((card) => {
     btn.addEventListener('click', () => {
         const cardClone = card.cloneNode(true);
         cartWrapper.appendChild(cardClone);
-        cartEmpty.remove();
         showData();
+
+        const removeBtn = cardClone.querySelector('.btn');
+        removeBtn.textContent = 'Удалить из корзины';
+        removeBtn.addEventListener('click', () => {
+            cardClone.remove();
+            showData();
+
+        });
     });
 });
 
 function showData() {
-    const cardsCart = cartWrapper.querySelectorAll('.card');
+    const cardsCart = cartWrapper.querySelectorAll('.card'),
+      cardsPrice = cartWrapper.querySelectorAll('.card-price'),
+      cardTotal = document.querySelector('.cart-total span');
+    let sum = 0;
     countGoods.textContent = cardsCart.length;
+    cardsPrice.forEach((cardPrice) => {
+    sum += parseInt(cardPrice.textContent);
+    });
+    cardTotal.textContent = sum;
     //console.log(cardsCart.length);
+    if (cardsCart.length !== 0) {
+        cartEmpty.remove();
+    } else {
+        cartWrapper.appendChild(cartEmpty);
+
+    };
 
 };
 
