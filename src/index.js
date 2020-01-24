bd76cf54fbcdd6f6d59f27c0be2faf686175d1ea
Oleg <52830599+u001055@users.noreply.github.com>
2020-01-25 00:51:20 +0300

    search text

--- src/index.js
+++ src/index.js
@@ -88,7 +88,12 @@ function addCart() {
 
 function actionPage() {
     const cards = document.querySelectorAll('.goods .card')
-    discountCheckbox = document.getElementById('discount-checkbox');
+    discountCheckbox = document.getElementById('discount-checkbox'),
+    min = document.getElementById('min'),
+    max = document.getElementById('max'),
+    search = document.querySelector('.search-wrapper_input'),
+    searchBtn = document.querySelector('.search-btn');
+
     discountCheckbox.addEventListener('click', () => {
         cards.forEach((card) => {
             if (discountCheckbox.checked) {
@@ -99,6 +104,37 @@ function actionPage() {
                 card.parentNode.style.display = '';
             };
         });
+    });
+
+    function filterPrice(){
+        cards.forEach((card) => {
+            const cardPrice = card.querySelector('.card-price'),
+            price = parseFloat(cardPrice.textContent);
+            if ((min.value && price < min.value) || (max.value && price > max.value)){
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = '';
+            }
+
+        });
+
+    };
+
+    max.addEventListener('change', filterPrice);
+    min.addEventListener('change', filterPrice);
+
+    searchBtn.addEventListener('click', () => {
+        const searchText = new RegExp(search.value.trim(), 'i');
+        cards.forEach((card) => {
+            const title = card.querySelector('.card-title');
+            if (!searchText.test(title.textContent)) {
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = '';
+            }
+        });
+        
+
     });
 
 };
